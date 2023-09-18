b8bd0b7d93ffd6b65eb636c93cb58ea0dcf9f06f
devgameby123 <83538604+devgameby123@users.noreply.github.com>
2023-09-18 18:52:26 +0700

    set route for mew

---
+++ src/app/mewtest/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../../../components/Button'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <Button/>
+  )
+}
+
+export default page
