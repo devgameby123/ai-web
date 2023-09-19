7ff864704998dbe14276c5c3656ecd3ed17a91c5
mimimew <rujira03312@gmail.com>
2023-09-20 01:14:16 +0700

    update
    versiopn 11%

--- src/app/mewtest/page.tsx
+++ src/app/mewtest/page.tsx
@@ -7,7 +7,7 @@ type Props = {}
 function page({}: Props) {
   return (
     <>
-      <Button className='btnNext'>next</Button>
+      <Button className='btnNext icon'> </Button>
       <Button className='btnAdd1'>Add to Favourite</Button>
       <button className='btnAdd2'><span>Add to Favourite</span></button>
       <Button className='btnCategory'>Fantacy</Button>
