060e29ecfbec86c0bd32443129a93241ad5c99cf
devgameby123 <83538604+devgameby123@users.noreply.github.com>
2023-09-20 17:59:57 +0700

    fix mew

--- components/ButtonIcon.tsx
+++ components/ButtonIcon.tsx
@@ -1,3 +1,11 @@
+import React from 'react'
 
+type Props = {}
 
+const ButtonIcon = (props: Props) => {
+  return (
+    <div>ButtonIcon</div>
+  )
+}
 
+export default ButtonIcon
--- src/app/mewtest/page.tsx
+++ src/app/mewtest/page.tsx
@@ -8,10 +8,7 @@ type Props = {}
 function page({}: Props) {
   return (
     <>
-      <Button className='btnNext icon'>next</Button>
-      <Button className='btnAdd1'>Add to Favourite</Button>
-      <button className='btnAdd2'><span>Add to Favourite</span></button>
-      <Tagbar className='name'>Name1</Tagbar>
+      <Button className='btnAdd1'>Click</Button>
     </>
     
   )
